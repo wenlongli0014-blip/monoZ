03c862c52e92e71165584c6776be93c5fb726923
Hong Gao <hong.gao@cern.ch>
2024-07-29 05:07:25 +0200

    the environment set for reweight proccedure

---
+++ env_reweight.sh
@@ -0,0 +1,7 @@
+# Consistent LCG environment (http://lcginfo.cern.ch)
+. /cvmfs/sft.cern.ch/lcg/views/setupViews.sh LCG_97python3 x86_64-centos7-gcc9-opt
+export SCRAM_ARCH=slc7_amd64_gcc920
+
+export HZZ2L2NU_BASE=$(pwd)
+export PYTHONPATH="${HZZ2L2NU_BASE}/python:$PYTHONPATH"
+export PATH="${HZZ2L2NU_BASE}/bin:${PATH}"
