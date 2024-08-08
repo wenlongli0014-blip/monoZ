c037fda18c9ac2baec512e417d6293d6a211940b
Hong Gao <hong.gao@cern.ch>
2024-08-08 04:41:48 +0200

    Update file GJets_highMET.sh

--- GJets_highMET.sh
+++ GJets_highMET.sh
@@ -21,15 +21,21 @@ cd InstrMETReweighting
 for YEAR in 2016HIPM 2016noHIPM 2017 2018; do python CollectWeights.py -y ${YEAR}; done
 cd ..
 #计算Data Driven DY在low MET 和 SR 的估计值
-for YEAR in 2016HIPM 2016noHIPM 2018; do python Data_Driven_DY_2018_2016.py -y ${YEAR}; done
-
+python Data_Driven_DY_2018.py
+for YEAR in 2016HIPM 2016noHIPM 2017; do python Data_Driven_DY.py -y ${YEAR}; done
 cd root
 hadd histo2d_2016_SR.root histo2d_2016HIPM_SR.root histo2d_2016noHIPM_SR.root 
 hadd histo2d_2016_lowMET.root histo2d_2016HIPM_lowMET.root histo2d_2016noHIPM_lowMET.root   
+hadd histo2d_2017_and_2018_lowMET.root histo2d_2017_lowMET.root histo2d_2018_lowMET.root 
 cd ../..
 mkdir batch_singlephoton_2016
 mkdir batch_singlephoton_2016/merged
 hadd batch_singlephoton_2016/merged/DYJetsToLL_Data-driven_GJets_nvtx_eta_pt_reweighted.root  batch_singlephoton_2016HIPM/merged/DYJetsToLL_Data-driven_GJets_nvtx_eta_pt_reweighted.root batch_singlephoton_2016noHIPM/merged/DYJetsToLL_Data-driven_GJets_nvtx_eta_pt_reweighted.root
+hadd batch_singlephoton_2017_and_2018/merged/DYJetsToLL_Data-driven_GJets_nvtx_eta_pt_reweighted.root  batch_singlephoton_2017/merged/DYJetsToLL_Data-driven_GJets_nvtx_eta_pt_reweighted.root batch_singlephoton_2018/merged/DYJetsToLL_Data-driven_GJets_nvtx_eta_pt_reweighted.root
 cd zz2l2nu_reweight
-#将pt_ll[60,82.5] MET>160区间的DY事例数替换成MC
-for YEAR in 2016 2018; do python replaceGJets.py -y ${YEAR}; done
+
+#将pt_ll[60,82.5] MET>160,200区间的DY事例数替换成MC
+for YEAR in 2016 2017; do python replaceGJets_2016_2017.py -y ${YEAR}; done
+python replaceGJets_2018.py -y 2018
+hadd histo2d_2017_and_2018_SR_final.root histo2d_2017_SR_final.root histo2d_2018_SR_final.root
+python final_plot.py
