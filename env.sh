61e75f36f68b24a690e693cec593b56f9c6bd339
yuji <yuji.li@cern.ch>
2025-10-15 16:30:58 +0200

    change env

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
-source /cvmfs/sft.cern.ch/lcg/views/setupViews.sh LCG_105 x86_64-centos9-gcc12-opt
+source /cvmfs/sft.cern.ch/lcg/views/setupViews.sh LCG_105 x86_64-el9-gcc12-opt
 export SCRAM_ARCH=el9_amd64_gcc12
 
 export HZZ2L2NU_BASE=$(pwd)
