9191c49cbfbb85509fdecb0c0857db4bb38bee8e
yuji <yuji.li@cern.ch>
2025-10-15 16:26:33 +0200

    reset env

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
-source /cvmfs/sft.cern.ch/lcg/views/setupViews.sh LCG_102 x86_64-centos9-gcc11-opt
-export SCRAM_ARCH=el9_amd64_gcc11
+source /cvmfs/sft.cern.ch/lcg/views/setupViews.sh LCG_105 x86_64-centos9-gcc12-opt
+export SCRAM_ARCH=el9_amd64_gcc12
 
 export HZZ2L2NU_BASE=$(pwd)
 export PYTHONPATH="${HZZ2L2NU_BASE}/python:$PYTHONPATH"
